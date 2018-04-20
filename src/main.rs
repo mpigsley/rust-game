bbde9429840971689971da240dabf038b9571a73
Derek Zeller <root.dzeller@gmail.com>
2018-04-19 21:55:01 -0500

    keyboard input

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,12 @@ const GAME_MAP: [[u8; 28]; 20] = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+const TILE_SIZE: f64 = 25.0;
+
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
+    up: bool, down: bool, left: bool, right: bool, // direction indicators
+    px: f64, py: f64
 }
 
 impl App {
@@ -42,24 +46,51 @@ impl App {
 
         const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const GREY: [f32; 4] = [0.5, 0.5, 0.5, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         self.gl.draw(args.viewport(), |c, gl| {
             clear(WHITE, gl);
 
+            // print map
             for (i, &row) in GAME_MAP.iter().enumerate() {
                 for (j, &game_piece) in row.iter().enumerate() {
                     if game_piece == 1 {
-                        let square = rectangle::square((j * 25) as f64, (i * 25) as f64, 25.0);
+                        let square = rectangle::square((j * 25) as f64, (i * 25) as f64, TILE_SIZE);
                         rectangle(GREY, square, c.transform, gl);
                     }
                 }
             }
+            // print player
+            // let square = rectangle::square((self.px * TILE_SIZE) as f64, (self.py * TILE_SIZE) as f64, 25.0);
+            // rectangle(RED, square, c.transform, gl);
         });
     }
 
-    // fn update(&mut self, args: &UpdateArgs) {
+    fn update(&mut self, _args: &UpdateArgs) {
+        // println!("{:?}", args);
 
-    // }
+    }
+    fn input(&mut self, _args: &ButtonArgs) {
+        println!("{:?}", _args.state);
+        println!("{:?}", _args.button);
+        println!("{:?}", _args.scancode);
+        match _args.button {
+            Button::Keyboard(Key::Up) => {
+                self.up =_args.state == ButtonState::Press;
+            },
+            Button::Keyboard(Key::Down) => {
+                self.down =_args.state == ButtonState::Press;
+            },
+            Button::Keyboard(Key::Left) => {
+                self.left =_args.state == ButtonState::Press;
+            },
+            Button::Keyboard(Key::Right) => {
+                self.right =_args.state == ButtonState::Press;
+            },
+            _ => {}
+
+        }
+    }
 }
 
 fn main() {
@@ -76,6 +107,8 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
+        up: false, down: false, left: false, right: false,
+        px: TILE_SIZE, py: (18.0 * TILE_SIZE) as f64,
     };
 
     let mut events = Events::new(EventSettings::new());
@@ -84,8 +117,12 @@ fn main() {
             app.render(&r);
         }
 
-        // if let Some(u) = e.update_args() {
-        //     app.update(&u);
-        // }
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
+
+        if let Some(b) = e.button_args() {
+            app.input(&b);
+        }
     }
 }
